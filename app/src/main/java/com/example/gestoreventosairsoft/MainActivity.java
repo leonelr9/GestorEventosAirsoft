d78c8558221f696d865c4cc23be2596425628dba
Leonel <leonelfilipe1@gmail.com>
2019-03-22 09:53:18 +0000

    Criar butoes

--- app/src/main/java/com/example/gestoreventosairsoft/MainActivity.java
+++ app/src/main/java/com/example/gestoreventosairsoft/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gestoreventosairsoft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostraMensagemEventos(View view) {
+        Toast.makeText(this, "Vai para a pagina de Eventos.", Toast.LENGTH_LONG).show();
+    }
+
+    public void mostraMensagemJogador(View view) {
+        Toast.makeText(this, "Vai para a pagina de Jogadores", Toast.LENGTH_LONG).show();
     }
 }
