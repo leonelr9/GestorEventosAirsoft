d38e14afd2364165f805ab4e8aae9c75ca4df2c9
Leonel <leonelfilipe1@gmail.com>
2019-03-27 19:06:32 +0000

    Criação de nova atividade

---
+++ app/src/main/java/com/example/gestoreventosairsoft/Jogador.java
@@ -0,0 +1,21 @@
+package com.example.gestoreventosairsoft;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Jogador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogador);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
