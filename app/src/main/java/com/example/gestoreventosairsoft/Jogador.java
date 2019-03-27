1e2f4332a26d2d0c9ea7a14e7b968b874abbf416
Leonel <leonelfilipe1@gmail.com>
2019-03-27 19:59:26 +0000

    Colocação de botoes na nova atividade

--- app/src/main/java/com/example/gestoreventosairsoft/Jogador.java
+++ app/src/main/java/com/example/gestoreventosairsoft/Jogador.java
@@ -1,9 +1,11 @@
 package com.example.gestoreventosairsoft;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Jogador extends AppCompatActivity {
 
@@ -11,11 +13,11 @@ public class Jogador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogador);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
 }
