3dba055e73473ad035348034ea80b311f2111605
Leonel <leonelfilipe1@gmail.com>
2019-04-04 23:43:37 +0100

    Visualização dos dados na nova atividade

---
+++ app/src/main/java/com/example/gestoreventosairsoft/DefinicoesApp.java
@@ -0,0 +1,9 @@
+package com.example.gestoreventosairsoft;
+
+public class DefinicoesApp {
+    public static final String NOME = "NOME";
+    public static final String CONTACTO = "CONTACTO";
+    public static final String DATANASCIMENTO = "DATANASCIMENTO";
+    public static final String GRUPOSANGUINEO = "GRUPOSANGUINEO";
+    public static final String NUMSOCIO = "NUMSOCIO";
+}
